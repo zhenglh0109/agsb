cb74dd5c82841c4391dd58e7b66dfd4b99c59fc8
zhenglh0109 <90875639+zhenglh0109@users.noreply.github.com>
2025-06-06 22:25:22 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -35,7 +35,7 @@ USER_NAME = "kkddytd"         # 用户名
 UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
 DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiYjMxMWIwMzQ0YzdhMGNiNTViNjU5ZmJhZTY4MmYyZDIiLCJ0IjoiNjYwN2M2MjAtZDNjNi00ZTVkLTg5YzAtYzEzNmZjM2ViZTI2IiwicyI6Ik5XTmtOR05sTmpjdFpEWTVZUzAwWTJOa0xXRmpOakl0TkRSaU5EQTNZakZqWlRjMSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+CF_TOKEN = "eyJhIjoiYjMxMWIwMzQ0YzdhMGNiNTViNjU5ZmJhZTY4MmYyZDIiLCJ0IjoiMzU3ZDE3ZTItYWU3Yi00ZmZkLWIyN2EtZTk5NzhlYTFlMTExIiwicyI6IlpUbGhNVFl3WXpBdFlqVXdOUzAwTmpNMkxUZzFNMlV0WlRnNVl6YzBNek0wTVdabCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
